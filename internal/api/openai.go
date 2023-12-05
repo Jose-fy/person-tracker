689951a7a2095b96935d35849b95917a690147b8
José Rodrigues <pedrogodinhoc@gmail.com>
2023-12-04 22:27:59 -0300

    Added OpenAI integration and unitests

---
+++ internal/api/openai.go
@@ -0,0 +1,113 @@
+package openai
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
+
+
+type OpenAIClient interface {
+    SendMessageGPT(message string) (OpenAIResponse, error)
+}
+
+
+type RealOpenAIClient struct {
+    HTTPClient *http.Client
+}
+
+
+func (c *RealOpenAIClient) SendMessageGPT(message string) (OpenAIResponse, error){
+
+	request := NewOpenAIRequest(message)
+	jsonData, err := json.Marshal(request)
+
+    if err != nil {
+        log.Print("Error Mashmallowing")
+        return OpenAIResponse{}, err // Return an error if JSON marshaling fails
+    }
+
+    apiKey := os.Getenv("OPENAI_API_KEY")
+
+	if apiKey == "" {
+		log.Fatal("API key not set in environment variables")
+    }
+
+	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bytes.NewBuffer(jsonData))
+
+	if err != nil {
+        log.Print("Unable to post http request")
+		return OpenAIResponse{}, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer " + apiKey)
+
+	resp, err := c.HTTPClient.Do(req)
+
+	defer resp.Body.Close()
+
+	var result OpenAIResponse
+	decoder := json.NewDecoder(resp.Body)
+
+	err = decoder.Decode(&result)
+
+	if err != nil {
+        return result, err
+    }
+
+    return result, err
+
+}
+
+
+type OpenAIResponse struct {
+    ID      string `json:"id"`
+    Object  string `json:"object"`
+    Created int    `json:"created"`
+    Model   string `json:"model"`
+    Choices []struct {
+        Index         int `json:"index"`
+        Message       struct {
+            Role    string `json:"role"`
+            Content string `json:"content"`
+        } `json:"message"`
+        FinishReason string `json:"finish_reason"`
+    } `json:"choices"`
+    Usage struct {
+        PromptTokens     int `json:"prompt_tokens"`
+        CompletionTokens int `json:"completion_tokens"`
+        TotalTokens      int `json:"total_tokens"`
+    } `json:"usage"`
+    SystemFingerprint interface{} `json:"system_fingerprint"` // null in JSON, so using interface{}
+}
+
+type OpenAIRequest struct {
+	Model string `json:"model"`
+	Messages []Message `json:"messages"`
+	Temperature float32 `json:"temperature"`
+
+}
+
+type Message struct {
+	Role string `json:"role"`
+	Content string `json:"content"`
+}
+
+
+func NewOpenAIRequest(message string) OpenAIRequest {
+    return OpenAIRequest{
+        Model: "gpt-3.5-turbo",
+        Messages: []Message{
+            {
+                Role: "user",
+                Content: "Hi say 'this is a test'",
+            },
+        },
+        Temperature: 0.7,
+    }
+}
+
+
---
+++ internal/api/openai_test.go
@@ -0,0 +1,64 @@
+package openai
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type MockOpenAIClient struct {
+	MockResponse OpenAIResponse
+	MockError    error
+}
+
+func (m *MockOpenAIClient) SendMessageGPT(message string) (OpenAIResponse, error) {
+	return m.MockResponse, m.MockError
+}
+
+func TestSomeFunction(t *testing.T) {
+	mockClient := MockOpenAIClient{
+		MockResponse: OpenAIResponse{
+			ID:      "chatcmpl-8RrXwXB28UGKENenrJMpqoB2nTLaQ",
+			Object:  "chat.completion",
+			Created: 1701651380,
+			Model:   "gpt-3.5-turbo-0613",
+			Choices: []struct {
+				Index   int `json:"index"`
+				Message struct {
+					Role    string `json:"role"`
+					Content string `json:"content"`
+				} `json:"message"`
+				FinishReason string `json:"finish_reason"`
+			}{
+				{
+					Index: 0,
+					Message: struct {
+						Role    string `json:"role"`
+						Content string `json:"content"`
+					}{
+						Role:    "assistant",
+						Content: "This is a test.",
+					},
+					FinishReason: "stop",
+				},
+			},
+			Usage: struct {
+				PromptTokens     int `json:"prompt_tokens"`
+				CompletionTokens int `json:"completion_tokens"`
+				TotalTokens      int `json:"total_tokens"`
+			}{
+				PromptTokens:     15,
+				CompletionTokens: 5,
+				TotalTokens:      20,
+			},
+			SystemFingerprint: nil,
+		},
+	}
+
+	message := "Say this is a test."
+	result, _ := mockClient.SendMessageGPT(message)
+
+	message_result := result.Choices[0].Message.Content
+	assert.Equal(t, message_result, "This is a test.", "Should say this is a test")
+
+}
